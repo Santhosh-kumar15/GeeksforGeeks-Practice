a33a18a4cf3bd31987bf0a8eb7dad86714597fc9
Santhoshkumar J V <santhoshkumarjv1512@gmail.com>
2024-09-01 18:46:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+
+
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            new Solution().segregate0and1(arr);
+            for (int i = 0; i < array.size(); i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            // System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    void segregate0and1(int[] arr) {
+        // code here
+        int mid = 0;
+        int low = 0;
+        while(mid < arr.length)
+        {
+            if(arr[mid] == 0)
+            {
+                swap(arr, mid, low);
+                mid++;
+                low++;
+            }
+            else
+                mid++;
+        }
+    }
+    public static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
