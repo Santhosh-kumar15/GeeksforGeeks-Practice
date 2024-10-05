e9e0aeede78f28722a565748ea8225fcfe27aee3
Santhoshkumar J V <santhoshkumarjv1512@gmail.com>
2024-10-05 09:35:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Perfect Numbers/perfect-numbers.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            long N = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.isPerfectNumber(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int isPerfectNumber(long N) {
+        // code here
+        long temp = N;
+        long sum = 0;
+        for(int i = 1; i < Math.sqrt(N); i++)
+        {
+            if(N % i == 0)
+            {
+                sum += i;
+                if(N != N/i)
+                    sum += N / i;
+            }
+        }
+        if(sum == temp)
+            return 1;
+        else
+            return 0;
+    }
+};
