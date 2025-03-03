a2277e4519b63dda4a29935fafdfb94e77e50ba2
Santhoshkumar J V <santhoshkumarjv1512@gmail.com>
2025-03-03 15:28:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.java
+++ Difficulty: Medium/Quick Sort/quick-sort.java
@@ -1,74 +1,73 @@
 //{ Driver Code Starts
+import java.io.*;
 import java.util.*;
-class Sorting
-{
-	static void printArray(int arr[])
-    {
+
+class Sorting {
+    static void printArray(int arr[]) {
         int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i < n; ++i) System.out.print(arr[i] + " ");
         System.out.println();
     }
-    
-    // Driver program
-    public static void main(String args[])
-    {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		while(T>0)
-		{
-			int n = sc.nextInt();
-			int arr[] = new int[n];
-			for(int i=0;i<n;i++)
-				arr[i] = sc.nextInt();
 
-			
-			new Solution().quickSort(arr,0,n-1);
-			printArray(arr);
-		    T--;
-		}
-} }
+    // Driver program
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().quickSort(arr, 0, n - 1);
+            printArray(arr);
+        }
+    }
+}
 // } Driver Code Ends
 
 
-class Solution
-{
-    //Function to sort an array using quick sort algorithm.
-    static void quickSort(int arr[], int low, int high)
-    {
+class Solution {
+    // Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high) {
         // code here
-        if(low<high){
-        int partitionIndex = partition(arr, low, high);
-        quickSort(arr, low, partitionIndex-1);
-        quickSort(arr, partitionIndex+1, high);
+        if(low < high)
+        {
+            int partitionIndex = partition(arr, low, high);
+            quickSort(arr, low, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, high);
         }
     }
-    static int partition(int arr[], int low, int high)
-    {
+
+    static int partition(int arr[], int low, int high) {
         // your code here
+        int pivot = arr[low];
         int i = low;
         int j = high;
-        int pivot = arr[high];
-        while(i<j)
+        while(i < j)
         {
-            while(arr[i] <= pivot && i <= high-1)
+            while(arr[i] <= pivot && i <= high - 1)
             {
                 i++;
             }
-            while(arr[j] >= pivot &&  j >= low+1)
+            while(arr[j] > pivot && j >= low + 1)
             {
                 j--;
             }
-            if(i<j)
+            if(i < j)
             {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
-        int temp = arr[high];
-        arr[high] = arr[i];
-        arr[i] = temp;
-        return i;
-    } 
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+        return j;
+        
+        
+    }
 }
